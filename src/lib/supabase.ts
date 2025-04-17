5a69a9dcb5790283b8acee582a7070c84440efe0
fbazn <knightssam@outlook.com>
2025-04-17 22:32:38 +0100

    Fix: Use createPagesBrowserClient for Supabase setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// lib/supabase.ts
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
-
-export default supabase
